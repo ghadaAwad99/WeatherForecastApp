3a802f1ef5a4b8eccbf9a40467b38263790b08df
Ghada Mustafa <74993384+ghadaAwad99@users.noreply.github.com>
2022-04-02 06:54:08 +0200

    room update

--- app/src/main/java/com/example/weatherforcast/database/FavDAO.kt
+++ app/src/main/java/com/example/weatherforcast/database/FavDAO.kt
@@ -1,11 +1,19 @@
 package com.example.weatherforcast.database
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.weatherforcast.model.FavoriteModel
 
 @Dao
 interface FavDAO {
     @Insert
     fun insertToFavorite(favoriteModel: FavoriteModel)
+
+    @Delete
+    fun deleteFromFavorite(favoriteModel: FavoriteModel)
+
+    @Query("SELECT * FROM favorite WHERE locality = :city")
+    fun getFavoritePlace(city:String)
 }
